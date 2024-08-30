3cb4d4e1e9ac8e5677a9d7943add2773cc6b069b
Andrew Hsu <100561529+andrewhsugithub@users.noreply.github.com>
2024-08-30 23:56:51 +0800

    Feat: add real3d as microservice (#2)
    * feat: turn Real3D into server
    
    * fix: add quotes in ffmpeg to process file names with whitespace
    
    ---------
    
    Co-authored-by: andrewhsugithub <andrewhsugithub>

---
+++ server/dto/__init__.py
@@ -0,0 +1,31 @@
+﻿from pydantic import BaseModel, Field
+from typing import Literal, Optional
+from enum import Enum
+
+class BlinkModeEnum(str, Enum):
+    none = "none"
+    period = "period"
+
+class OutModeEnum(str, Enum):
+    final = "final"
+    concat_debug = "concat_debug"
+
+class Real3DRequest(BaseModel):
+    a2m_ckpt: Optional[str] = Field(default='checkpoints/240210_real3dportrait_orig/audio2secc_vae', description='Path to A2M checkpoint')
+    head_ckpt: Optional[str] = Field(default='', description='Path to head checkpoint')
+    torso_ckpt: Optional[str] = Field(default='checkpoints/240210_real3dportrait_orig/secc2plane_torso_orig', description='Path to torso checkpoint')
+    ############### Input Args ################# 
+    src_img: Optional[str] = Field(default='data/raw/examples/Macron.png', description='Path to source image')
+    bg_img: Optional[str] = Field(default='', description='Path to background image')
+    drv_aud: Optional[str] = Field(default='data/raw/examples/Obama_5s.wav', description='Path to driving audio file')
+    drv_pose: Optional[str] = Field(default='data/raw/examples/May_5s.mp4', description='Path to driving pose video')
+    blink_mode: Optional[BlinkModeEnum] = Field(default=BlinkModeEnum.period, description='Blink mode: none or period')
+    temperature: Optional[float] = Field(default=0.2, description='sampling temperature in audio2motion, higher -> more diverse, less accurate')
+    mouth_amp: Optional[float] = Field(default=0.45, description='scale of predicted mouth, enabled in audio-driven')
+    head_torso_threshold: Optional[float] = Field(default=None, description="0.1~1.0, turn up this value if the hair is translucent")
+    out_name: Optional[str] = Field(default='', description='Output filename')
+    out_mode: Optional[OutModeEnum] = Field(default=OutModeEnum.concat_debug, description='final: only output talking head video; concat_debug: talking head with internel features')
+    map_to_init_pose: bool = Field(default=True, description='Whether to map the pose of the first frame to source image')
+    seed: Optional[int] = Field(default=None, description='random seed, default None to use time.time()')
+    min_face_area_percent: Optional[float] = Field(default=0.2, description='scale of predicted mouth, enabled in audio-driven')
+    low_memory_usage: bool = Field(default=False, description='write img to video upon generated, leads to slower fps, but use less memory')
---
+++ server/server.py
@@ -0,0 +1,37 @@
+﻿from fastapi import FastAPI
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+from server.routes.real3d import router
+
+
+def init_routers(app_: FastAPI) -> None:
+    real3d_router = router
+    app_.include_router(real3d_router)
+
+def make_middleware() -> list[Middleware]:
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        ),
+    ]
+    return middleware
+
+def create_app() -> FastAPI:
+    app_ = FastAPI(
+        title="Hide",
+        description="Hide API",
+        version="1.0.0",
+        docs_url="/docs",
+        redoc_url="/redoc",
+        middleware=make_middleware(),
+    )
+    init_routers(app_=app_)
+    return app_
+
+
+app = create_app()
