8bbcd7e126b87c0fc2d6e291ece3d1f98ca11e11
andrewhsugithub <andrew1mail@gmail.com>
2024-10-27 08:08:27 +0800

    feat: integrate with frontend

--- server/routes/real3d.py
+++ server/routes/real3d.py
@@ -1,5 +1,5 @@
 ﻿from fastapi import APIRouter, HTTPException, Request
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from inference.real3d_infer import GeneFace2Infer
 
 from server.dto import Real3DRequest
@@ -30,15 +30,23 @@ router = APIRouter()
 
 @router.post("/real3d/")
 async def real3d(req: Real3DRequest, request: Request) -> FileResponse:
-    access_token = request.headers['Authorization'].split(' ')[1]
-    payload = jwt.decode(access_token, public_key, algorithms=["RS256"])
-    user_id = payload['sub']
+    try:
+        access_token = request.headers['Authorization'].split(' ')[1]
+        payload = jwt.decode(access_token, public_key, algorithms=["RS256"])
+    except jwt.exceptions.ExpiredSignatureError:
+        return JSONResponse(content={"message": "Token has expired"}, status_code=401)
+        
+    user_id = req.user_id
+    emotion = req.emotion
+    filename = req.drv_aud.split('/')[-1][:-4]
     
     args = req.copy(deep=True)
+    #! should not be hard coded here
     inp = {
         'a2m_ckpt': args.a2m_ckpt,
         'head_ckpt': args.head_ckpt,
         'torso_ckpt': args.torso_ckpt,
+        # 'src_image_name': f"/mnt/Nami/users/Jason0411202/buckets/{user_id}/image/{emotion}/{emotion}.png",
         'src_image_name': args.src_img,
         'bg_image_name': args.bg_img,
         'drv_audio_name': args.drv_aud,
@@ -46,6 +54,7 @@ async def real3d(req: Real3DRequest, request: Request) -> FileResponse:
         'blink_mode': args.blink_mode,
         'temperature': args.temperature,
         'mouth_amp': args.mouth_amp,
+        'out_name': f"/mnt/Nami/users/Jason0411202/buckets/{user_id}/Real3D/{filename}.mp4",
         'out_name': args.out_name,
         'out_mode': args.out_mode,
         'map_to_init_pose': args.map_to_init_pose,
@@ -55,8 +64,8 @@ async def real3d(req: Real3DRequest, request: Request) -> FileResponse:
         'low_memory_usage': args.low_memory_usage,
         }
     
-    date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    inp['out_name'] = f"/mnt/Nami/users/Jason0411202/buckets/{user_id}/Real3D/{date}.mp4"
+    # date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    # inp['out_name'] = f"/mnt/Nami/users/Jason0411202/buckets/{user_id}/Real3D/{date}.mp4"
 
     GeneFace2Infer.example_run(inp)
     
@@ -64,4 +73,4 @@ async def real3d(req: Real3DRequest, request: Request) -> FileResponse:
         
     response = FileResponse(vid_path)
 
-    return response
+    return vid_path
